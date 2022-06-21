9f85e1d0dbb1c5d13450e662792e7ac18e85ef28
Jataz <you@example.com>
2022-06-21 23:24:57 +0200

    Second commit

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -164,18 +164,7 @@
             <!-- Content Header (Page header) -->
             <div class="content-header">
                 <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0 text-dark">@yield('pageName')</h1>
-                            gghhj
-                        </div><!-- /.col -->
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active">@yield('title')</li>
-                            </ol>
-                        </div><!-- /.col -->
-                    </div><!-- /.row -->
+                   @yield('content')
                 </div><!-- /.container-fluid -->
             </div>
             <!-- /.content-header -->
